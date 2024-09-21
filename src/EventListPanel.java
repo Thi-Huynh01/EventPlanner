685f117907f39647c6cf3ecab5721b2b1d9e095f
Thi-Huynh01 <huynhthi265@gmail.com>
2024-09-20 19:51:01 -0500

    Implemented JCheckBox

--- src/EventListPanel.java
+++ src/EventListPanel.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputMethodListener;
 import java.util.ArrayList;
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class EventListPanel extends JPanel {
     public ArrayList<Event> events;
@@ -10,32 +15,69 @@ public class EventListPanel extends JPanel {
     public JComboBox sortDropDown;
     public JCheckBox filterDisplay;
     public JButton addEventButton;
+    final int width = 800;
+    final int height = 600;
+    public JTextField textField;
+    final String[] sortOptions = {"Select Filter", "Name", "Date"};
 
     public EventListPanel() {
-        setPreferredSize(new Dimension(800, 600));
+        textField = new JTextField();
+        setPreferredSize(new Dimension(800, 800));
         setBackground(Color.pink);
         events = new ArrayList<>();
 
+        displayPanel = new JPanel();
+        //displayPanel.setBackground(Color.gray);
+        displayPanel.setPreferredSize(new Dimension(700, 400));
+
         controlPanel = new JPanel();
-        controlPanel.setPreferredSize(new Dimension(700, 500));
+        controlPanel.setPreferredSize(new Dimension(700, 200));
+
         add(controlPanel);
+        add(displayPanel);
 
         addEventButton = new JButton("Add Event");
         addEventButton.setFont(new Font("Tahoma", Font.BOLD, 12));
         addEventButton.addActionListener(e -> {
-            //addEvent(new Meeting("CS CLUB",LocalDateTime.now(), LocalDateTime.now(), "Batesville"));
+
+            addEvent(new Meeting("DEF",LocalDateTime.now(), LocalDateTime.now(), "Conway"));
+            addEvent(new Meeting("ABC",LocalDateTime.now(), LocalDateTime.now(), "Batesville"));
+
         });
 
         controlPanel.add(addEventButton);
-        displayPanel = new JPanel();
+        filterDisplay = new JCheckBox();
 
-        //sortDropDown = new JComboBox();
-        //filterDisplay = new JCheckBox();
+        sortDropDown = new JComboBox(sortOptions);
+        sortDropDown.setFont(new Font("Ubuntu", Font.BOLD, 12));
+        sortDropDown.addActionListener(e -> {
+            if (sortDropDown.getSelectedItem().equals(sortOptions[1])) {
+                Collections.sort(events);
+            }
+            if (sortDropDown.getSelectedItem().equals("Date")) {
+
+            }
+            updateDisplay();
+        });
+        controlPanel.add(sortDropDown);
+
+
+        controlPanel.add(filterDisplay);
     }
 
     public void addEvent(Event event) {
         events.add(event);
-
+        updateDisplay();
     }
 
+    public void updateDisplay() {
+        displayPanel.removeAll();
+
+        for (Event event : events) {
+            displayPanel.add(new EventPanel(event));
+        }
+
+        revalidate();
+        repaint();
+    }
 }
--- src/EventPlanner.java
+++ src/EventPlanner.java
@@ -6,7 +6,7 @@ public class EventPlanner {
     public static void addDefaultEvents (EventPanel events) {
         Deadline deadL = new Deadline("Project", LocalDateTime.of(2024, 9, 25, 15, 0));
         Meeting meet = new Meeting("OOP w/Java", LocalDateTime.of(2024, 9, 20, 15,0), LocalDateTime.of(2024,9,20, 15, 50,0), "Room 339");
-
+        //EventPanel met = new EventPanel(meet);
     }
 
     public static void main(String[] args) {
